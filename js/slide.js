f0cb930d6ec8a4f89d925575409a4df0678b15da
ashik927 <ashik1012@gmail.com>
2020-10-07 22:54:49 +0600

    semi update

---
+++ js/slide.js
@@ -0,0 +1,46 @@
+
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $('.carousel-item').length;
+ 
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:false,
+        navigationText:["",""],
+        autoPlay:true
+    });
+});
+
+$(document).ready(function(){
+    $("#testimonial_slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["<img src='../images/Desktop/icon/next1.png' alt='previous' class='img-fluid'","<img src='../images/Desktop/icon/next2.png'  alt='previous' class='img-fluid'"],
+        autoPlay:true
+    });
+});
